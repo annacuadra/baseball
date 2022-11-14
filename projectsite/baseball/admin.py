f99ccf5818c1bfa3594cd20a76919469fa915dc4
annacuadra <202080161@psu.palawan.edu.ph>
2022-11-14 17:10:17 +0800

    changes made

--- projectsite/baseball/admin.py
+++ projectsite/baseball/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from django.contrib.admin import display
-from .models import Position
+from .models import Position, Person, Club, Play
 
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
     list_display = ("description",)
     search_fields = ("description",)
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("lastname","firstname","height","weight")
+    search_fields = ("lastname",)
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ("name","coach",)
+    search_fields = ("name",)
+
+@admin.register(Play)
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ("player","team","string_no","isActive",)
